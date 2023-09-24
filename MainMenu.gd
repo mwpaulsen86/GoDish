a254318f0b6104fecc96a74bb956902c5891baac
paulsenm <mwpaulsen86@gmail.com>
2023-09-23 22:31:50 -0700

    adding UI to Set IP button

---
+++ MainMenu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_set_ip_pressed():
+	print("Set ip pressed")
+	get_tree().change_scene_to_file("res://SetIPMenu.tscn")
+	pass # Replace with function body.
