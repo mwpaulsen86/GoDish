da194cc31e8389b5bc8504ecfca8655f00275af4
Michael <mwpaulsen86@gmail.com>
2023-09-24 11:25:17 -0700

    working on setting ip functionality

--- MainMenu.gd
+++ MainMenu.gd
@@ -1,4 +1,7 @@
 extends Control
+@export var byte1nums = ["10", "172", "196"]
+@export var byte2nums10 = ["11", "12", "110", "111"]
+@export var byte2nums172 = ["16"]
 
 
 # Called when the node enters the scene tree for the first time.
@@ -12,6 +15,5 @@ func _process(delta):
 
 
 func _on_set_ip_pressed():
-	print("Set ip pressed")
 	get_tree().change_scene_to_file("res://SetIPMenu.tscn")
 	pass # Replace with function body.
--- SetIPFromConfig.gd
+++ SetIPFromConfig.gd
@@ -3,6 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var something = Global.testNum
 	pass # Replace with function body.
 
 
@@ -10,7 +11,20 @@ func _ready():
 func _process(delta):
 	pass
 
+func _make_IP_block_button(block):
+	var ipButton = Button.new()
+	ipButton.text = block
+	ipButton.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	ipButton.size_flags_vertical = Control.SIZE_EXPAND_FILL
+	get_node("ipEntryContainer").add_child(ipButton)
+	pass
+
+func _make_buttons_for_IP_blocks(blocks):
+	for block in blocks:
+		_make_IP_block_button(block)
+	pass
 
 func _on_byte_1_pressed():
-	
+	#get_node("HBoxByte1").visible = true
+	_make_buttons_for_IP_blocks(Global.byte1nums)
 	pass # Replace with function body.
