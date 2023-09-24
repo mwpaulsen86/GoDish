ee0a7ff76e76d52d7d6cda83c1b36d3e3b3236ca
paulsenm <mwpaulsen86@gmail.com>
2023-09-24 15:38:51 -0700

    able to retrieve byte/block information

--- SetIPFromConfig.gd
+++ SetIPFromConfig.gd
@@ -11,20 +11,38 @@ func _ready():
 func _process(delta):
 	pass
 
-func _make_IP_block_button(block):
+func _make_IP_block_button(byte, block):
 	var ipButton = Button.new()
-	ipButton.text = block
+	Global.currentByte = byte
+	#var buttonName = "byte" + byte + "block" + block
+	#Global.customData.add 
+	ipButton.text = byte
 	ipButton.size_flags_horizontal = Control.SIZE_EXPAND_FILL
 	ipButton.size_flags_vertical = Control.SIZE_EXPAND_FILL
+	ipButton.pressed.connect(on_IP_block_button_pressed.bind(byte, block))
+	
 	get_node("ipEntryContainer").add_child(ipButton)
 	pass
 
-func _make_buttons_for_IP_blocks(blocks):
-	for block in blocks:
-		_make_IP_block_button(block)
+func _make_buttons_for_IP_blocks(bytes, block):
+	for byte in bytes:
+		print(byte)
+		_make_IP_block_button(byte, block)
 	pass
 
 func _on_byte_1_pressed():
 	#get_node("HBoxByte1").visible = true
-	_make_buttons_for_IP_blocks(Global.byte1nums)
+	Global.currentBlock = "1"
+	_make_buttons_for_IP_blocks(Global.byte1nums, "1")
 	pass # Replace with function body.
+
+func on_IP_block_button_pressed(byte, block):
+	
+	var blockName = "block" + Global.currentBlock
+	var byteName = "byte" + Global.currentByte
+	
+	#print("blockname: " + blockName)
+	#print("bytename: " + byteName)
+	print(byte)
+	#print("byte pressed was: " + get_node(byteName).)
+	pass
--- main.gd
+++ main.gd
@@ -3,6 +3,11 @@ extends Node
 
 var testNum = 4
 
+var customData = {}
+
 var byte1nums = ["10", "172", "196"]
 var byte2nums10 = ["11", "12", "110", "111"]
 var byte2nums172 = ["16"]
+
+var currentBlock = ""
+var currentByte = ""
