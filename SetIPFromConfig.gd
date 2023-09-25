63e1f3247efb78c2cba37d0a7a7c837df9961aee
Michael <mwpaulsen86@gmail.com>
2023-09-24 21:37:48 -0700

    Working on numpad functionality

--- SetIPFromConfig.gd
+++ SetIPFromConfig.gd
@@ -3,6 +3,9 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var NumPad = get_node("NumPadContainer")
+	for button in NumPad.get_children():
+		button.connect("pressed", Callable(self,"_on_numpad_button_pressed").bind(button.text))
 	pass # Replace with function body.
 
 
@@ -41,7 +44,63 @@ func _on_byte_1_pressed():
 	Global.currentOctetPos = "1"
 	_make_buttons_for_IP_blocks(Global.byte1nums, "1")
 	pass # Replace with function body.
+	
+
 
 func on_IP_block_button_pressed(octetVal, octetPos):
+	var octetNodeName = "MarginContainer/HBoxContainer/byte" + octetPos
+	var octetNode = get_node(octetNodeName)
+	octetNode.text = octetVal
 	print(octetVal)
 	pass
+
+
+func _on_byte_2_pressed():
+	Global.currentOctetPos = "2"
+	_make_buttons_for_IP_blocks(Global.byte2nums, "2")
+	pass # Replace with function body.
+
+func _on_byte_3_pressed():
+	Global.currentOctetPos = "3"
+	_make_buttons_for_IP_blocks(Global.byte3nums, "3")
+	pass # Replace with function body.
+
+func _on_numpad_button_pressed(text):
+	print(text)
+	pass
+
+func _on_num_pad_1_pressed():
+	
+	pass # Replace with function body.
+
+
+func _on_num_pad_2_pressed():
+	pass # Replace with function body.
+
+
+func _on_num_pad_3_pressed():
+	pass # Replace with function body.
+
+
+func _on_num_pad_4_pressed():
+	pass # Replace with function body.
+
+
+func _on_num_pad_5_pressed():
+	pass # Replace with function body.
+
+
+func _on_num_pad_6_pressed():
+	pass # Replace with function body.
+
+
+func _on_num_pad_7_pressed():
+	pass # Replace with function body.
+
+
+func _on_num_pad_8_pressed():
+	pass # Replace with function body.
+
+
+func _on_num_pad_9_pressed():
+	pass # Replace with function body.
--- main.gd
+++ main.gd
@@ -6,7 +6,7 @@ var testNum = 4
 var customData = {}
 
 var byte1nums = ["10", "172", "192"]
-var byte2nums10 = ["11", "12", "110", "111"]
-var byte2nums172 = ["16"]
+var byte2nums = ["11", "12", "16", "110", "111"]
+var byte3nums = ["4", "7", "12", "14", "15", "23", "25", "48", "67", "81", "89", "106", "137", "201"]
 
 var currentOctetPos = ""
