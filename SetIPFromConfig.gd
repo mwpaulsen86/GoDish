5d5303284df670db69cbaec2bec21bb50c54cf67
Michael <mwpaulsen86@gmail.com>
2023-09-26 21:31:17 -0700

    Numpad working mostly, getting ready to combine on_byte_#_pressed functions

--- SetIPFromConfig.gd
+++ SetIPFromConfig.gd
@@ -1,11 +1,13 @@
 extends Control
 
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var NumPad = get_node("NumPadContainer")
 	for button in NumPad.get_children():
 		button.connect("pressed", Callable(self,"_on_numpad_button_pressed").bind(button.text))
+	NumPad.visible = false
 	pass # Replace with function body.
 
 
@@ -65,42 +67,84 @@ func _on_byte_3_pressed():
 	_make_buttons_for_IP_blocks(Global.byte3nums, "3")
 	pass # Replace with function body.
 
+func _on_byte_4_pressed():
+	Global.currentOctetPos = "4"
+	_show_numpad()
+	pass # Replace with function body.
+
 func _on_numpad_button_pressed(text):
 	print(text)
+	_octet_string_manual_entry(text)
 	pass
-
-func _on_num_pad_1_pressed():
 	
-	pass # Replace with function body.
+func _octet_string_manual_entry(numString):
+	var octetToEdit = get_node("MarginContainer/HBoxContainer/byte" + Global.currentOctetPos)
+	var octetLength = octetToEdit.text.length()
+	if octetToEdit.text == "000" && numString.is_valid_int():
+		octetToEdit.text = numString
+	if octetLength < 3 && numString.is_valid_int():
+		octetToEdit.text += numString
+	elif numString == "Clr":
+		octetToEdit.text = ""
+	pass
+	
+func _show_numpad():
+	var NumPad = get_node("NumPadContainer")
+	NumPad.visible = true
+	pass
+	
+func _hide_numpad():
+	var NumPad = get_node("NumPadContainer")
+	NumPad.visible = false
+	pass
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+#
+#func _on_num_pad_1_pressed():
+#
+#	pass # Replace with function body.
+#
+#
+#func _on_num_pad_2_pressed():
+#	pass # Replace with function body.
+#
+#
+#func _on_num_pad_3_pressed():
+#	pass # Replace with function body.
+#
+#
+#func _on_num_pad_4_pressed():
+#	pass # Replace with function body.
+#
+#
+#func _on_num_pad_5_pressed():
+#	pass # Replace with function body.
+#
+#
+#func _on_num_pad_6_pressed():
+#	pass # Replace with function body.
+#
+#
+#func _on_num_pad_7_pressed():
+#	pass # Replace with function body.
+#
+#
+#func _on_num_pad_8_pressed():
+#	pass # Replace with function body.
+#
+#
+#func _on_num_pad_9_pressed():
+#	pass # Replace with function body.
 
 
-func _on_num_pad_2_pressed():
-	pass # Replace with function body.
 
-
-func _on_num_pad_3_pressed():
-	pass # Replace with function body.
-
-
-func _on_num_pad_4_pressed():
-	pass # Replace with function body.
-
-
-func _on_num_pad_5_pressed():
-	pass # Replace with function body.
-
-
-func _on_num_pad_6_pressed():
-	pass # Replace with function body.
-
-
-func _on_num_pad_7_pressed():
-	pass # Replace with function body.
-
-
-func _on_num_pad_8_pressed():
-	pass # Replace with function body.
-
-
-func _on_num_pad_9_pressed():
-	pass # Replace with function body.
--- main.gd
+++ main.gd
@@ -9,4 +9,5 @@ var byte1nums = ["10", "172", "192"]
 var byte2nums = ["11", "12", "16", "110", "111"]
 var byte3nums = ["4", "7", "12", "14", "15", "23", "25", "48", "67", "81", "89", "106", "137", "201"]
 
-var currentOctetPos = ""
+var currentOctetPos = "1"
+var gatewayIP = ["0", "0", "0", "0"]
